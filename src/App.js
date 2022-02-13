d85d4751f18886a2c615107a124ed3a68c8c93fb
Pridesd <whwotjr98@naver.com>
2022-02-13 19:59:03 +0900

    map & filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import React, { Component } from "react";
-import ScrollBox from "./05/ScrollBox";
+import IterationSample from "./06/IterationSample";
 
 // const App = () => {
 //   return (
@@ -16,10 +16,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
+        <IterationSample />
       </div>
     );
   }
