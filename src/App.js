0a59f2471eaba9a7815359e0bec6f60b3ee11be6
Pridesd <whwotjr98@naver.com>
2022-02-13 18:57:46 +0900

    ref

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 
-import React from "react";
-import EventPractice from "./EventPractice_function";
+import React, { Component } from "react";
+import ScrollBox from "./05/ScrollBox";
 
-const App = () => {
-  return <EventPractice />;
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+//       <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
+//     </div>
+//   );
+// };
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨밑으로
+        </button>
+      </div>
+    );
+  }
+}
 export default App;
