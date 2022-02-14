5fc936a05d94eae262cc3eeebdb6d196c0800bf8
Pridesd <whwotjr98@naver.com>
2022-02-14 16:48:41 +0900

    React LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 
 import React, { Component } from "react";
-import IterationSample from "./06/IterationSample";
+import LifeCycleSample from "./07/LifeCycleSample";
+import ErrorBoundary from "./07/ErrorBoundary";
 
 // const App = () => {
 //   return (
@@ -12,11 +13,28 @@ import IterationSample from "./06/IterationSample";
 //   );
 // };
 
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
