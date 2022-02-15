0a6217344059f2117bf0ca2f3d715bd2214a708f
Pridesd <whwotjr98@naver.com>
2022-02-15 16:29:34 +0900

    Hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
 import "./App.css";
 
-import React, { Component } from "react";
-import LifeCycleSample from "./07/LifeCycleSample";
-import ErrorBoundary from "./07/ErrorBoundary";
+import React, { Component, useState } from "react";
+import Average from "./08/Average";
 
-// const App = () => {
-//   return (
-//     <div>
-//       <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-//       <button onClick={() => this.scrollBox.scrollToBottom()}>맨밑으로</button>
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div>
+      <Average />
+    </div>
+  );
+};
 
-function getRandomColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
+// class App extends Component {
+//   state = {
+//     color: "#000000",
+//   };
 
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor(),
+//     });
+//   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>랜덤색상</button>
+//         <ErrorBoundary>
+//           <LifeCycleSample color={this.state.color} />
+//         </ErrorBoundary>
+//       </div>
+//     );
+//   }
+// }
 export default App;
