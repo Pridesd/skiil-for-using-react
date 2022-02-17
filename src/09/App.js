fc13073fc9d05df6958918ecdfcce374ba28b926
Pridesd <whwotjr98@naver.com>
2022-02-17 13:45:42 +0900

    CSS

---
+++ src/09/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import StyledComponent from "./StyledComponent";
+
+function App() {
+  return (
+    <div>
+      <StyledComponent />
+    </div>
+  );
+}
+
+export default App;
